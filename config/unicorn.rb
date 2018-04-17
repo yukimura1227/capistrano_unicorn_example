6e2ea6a8a54e20473cd670099c7384db6adcee35
yukimura1227 <takamura1227@gmail.com>
2018-04-17 22:53:47 +0900

    fix unicorn setting ref:https://qiita.com/zaru/items/9e02706b27094ce36902

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,42 +1,29 @@
-# paths
 rails_root = File.expand_path('../', __FILE__)
-app_path = rails_root
-working_directory app_path
-pid File.expand_path('../tmp/pids/unicorn.pid', __FILE__)
+working_directory rails_root + '/current'
 
-# listen
-listen '/tmp/unicorn.socket', :backlog => 64
-
-# logging
-stderr_path 'log/unicorn.stderr.log'
-stdout_path 'log/unicorn.stdout.log'
-
-# workers
-worker_processes 3
-
-# use correct Gemfile on restarts
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
-end
-
-# preload
+worker_processes 2
 preload_app true
+timeout 30
+listen '/tmp/unicorn.sock', :backlog => 64
+pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+
+stderr_path "#{rails_root}/log/unicorn.stderr.log"
+stdout_path "#{rails_root}/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', ENV['RAILS_ROOT'])
+end
+
+before_fork do |server, worker|
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
   end
 
-  # Before forking, kill the master process that belongs to the .oldbin PID.
-  # This enables 0 downtime deploys.
   old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
     end
   end
 end
